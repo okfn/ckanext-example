151a4adc8543f668c797eeb26d469aa81cde1fb1
John Glover <j@johnglover.net>
2012-01-31 12:18:46 +0000

    [setup] tidy up

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
-import sys, os
 
 version = '0.2'
 
 setup(
 	name='ckanext-example',
 	version=version,
-	description="Example extension for customising CKAN",
-	long_description="""\
-	""",
+	description='Example extension for customising CKAN',
+	long_description='',
 	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
 	keywords='',
 	author='Seb Bacon',
